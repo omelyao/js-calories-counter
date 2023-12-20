8fd9b1044c35ea477535108f4ab5bcaac1d232e0
Kolya <ryt9992@mail.ru>
2023-12-20 17:22:03 +0500

    gh-pages

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import CaloriesCounter from './modules/CaloriesCounter';
-
-const counterElements = document.querySelectorAll('.counter');
-
-counterElements.forEach(elem => {
-  const counter = new CaloriesCounter(elem);
-  counter.init();
-});
